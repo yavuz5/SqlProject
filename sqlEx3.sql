58bfa7ea107774b7fa3664d6538884b118fd559f
Yavuz Selim Aktuğ <95957459+yavuz5@users.noreply.github.com>
2022-07-27 16:49:06 +0300

    Create sqlEx3.sql

---
+++ sqlEx3.sql
@@ -0,0 +1,12 @@
+SELECT country FROM country
+WHERE country ~~'A%a'
+
+SELECT country FROM country
+WHERE country LIKE '______%n';
+
+SELECT title FROM film
+WHERE title ILIKE '&t&t&t&t';
+
+SELECT * FROM film
+WHERE title LIKE 'C&' AND length >90 AND rental_rate = 2.99;
+
